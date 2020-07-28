3171fd2d9b70059ee29d964d87df2aca597c153f
Jason Rice <jrice@entrata.com>
2020-07-28 10:49:05 -0600

    Functional lean React table with sorting and filtering. Included a bunch of storybooks as an example.

--- src/components/Table/storybook/Table.story.js
+++ src/components/Table/storybook/Table.story.js
@@ -6,13 +6,7 @@ import ThirdStoryPage from "./ThirdStoryPage";
 import { TableSorting } from "./TableSorting";
 
 storiesOf("Table", module)
-  .add("Default", () => (
-    <FirstStoryPage />
-  ))
-  .add("Multi-level Filtering", () => (
-    <SecondStoryPage />
-  ))
-  .add("Default sorting with custom data types (date)", () => (
-    <ThirdStoryPage />
-  ))
-  .add("Table Sorting using column configuration", () => <TableSorting />);
+  .add("Default", () => <FirstStoryPage />)
+  .add("Multi-level Filtering", () => <SecondStoryPage />)
+  .add("Default sorting with custom data types (date)", () => <ThirdStoryPage />)
+  .add("Table with links", () => <TableSorting />);
